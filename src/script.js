98387de9372ae125e4d1deec07f96f0e2d048b60
tan-sd <shengdatan@gmail.com>
2024-02-29 09:15:50 +0200

    Initialise Threejs Scene

--- src/script.js
+++ src/script.js
@@ -1,101 +1,166 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-import GUI from 'lil-gui'
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import GUI from "lil-gui";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import coffeeSmokeVertexShader from "./shaders/coffeeSmoke/vertex.glsl";
+import coffeeSmokeFragmentShader from "./shaders/coffeeSmoke/fragment.glsl";
 
 /**
  * Base
  */
 // Debug
-const gui = new GUI()
+const gui = new GUI();
+
+// Smoke Color
+let params = {
+    smokeColor: { r: 0.6, g: 0.3, b: 0.2 }
+}
+const smokeColorControl = gui
+    .addColor(params, "smokeColor")
+    .name("Smoke Color");
+smokeColorControl.onChange(() => {
+    smokeMaterial.uniforms.uColor.value.set(
+        params.smokeColor.r,
+        params.smokeColor.g,
+        params.smokeColor.b
+    );
+});
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 // Loaders
-const textureLoader = new THREE.TextureLoader()
-const gltfLoader = new GLTFLoader()
+const textureLoader = new THREE.TextureLoader();
+const gltfLoader = new GLTFLoader();
 
 /**
  * Sizes
  */
 const sizes = {
     width: window.innerWidth,
-    height: window.innerHeight
-}
+    height: window.innerHeight,
+};
 
-window.addEventListener('resize', () =>
-{
+window.addEventListener("resize", () => {
     // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
 
     // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
 
     // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+window.addEventListener("mousemove", (e) => {
+    // Update mouse position
+    const mouse = new THREE.Vector2();
+    mouse.x = (e.clientX / sizes.width) * 2 - 1;
+    mouse.y = -(e.clientY / sizes.height) * 2 + 1;
+    smokeMaterial.uniforms.uMouse.value = mouse;
+});
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(25, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 8
-camera.position.y = 10
-camera.position.z = 12
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(
+    25,
+    sizes.width / sizes.height,
+    0.1,
+    100
+);
+camera.position.x = 8;
+camera.position.y = 10;
+camera.position.z = 12;
+scene.add(camera);
 
 // Controls
-const controls = new OrbitControls(camera, canvas)
-controls.target.y = 3
-controls.enableDamping = true
+const controls = new OrbitControls(camera, canvas);
+controls.target.y = 2;
+controls.enableDamping = true;
+controls.enableZoom = false;
+controls.maxPolarAngle = Math.PI / 2;
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
-    antialias: true
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    antialias: true,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
  * Model
  */
-gltfLoader.load(
-    './bakedModel.glb',
-    (gltf) =>
-    {
-        gltf.scene.getObjectByName('baked').material.map.anisotropy = 8
-        scene.add(gltf.scene)
-    }
-)
+gltfLoader.load("./bakedModel.glb", (gltf) => {
+    gltf.scene.getObjectByName("baked").material.map.anisotropy = 8;
+    scene.add(gltf.scene);
+});
+
+/**
+ * Smoke
+ */
+// Geometry
+const smokeGeometry = new THREE.PlaneGeometry(1, 1, 16, 64);
+smokeGeometry.translate(0, 0.5, 0);
+smokeGeometry.scale(1.5, 6, 1.5);
+
+// Perlin texture
+const perlinTexture = textureLoader.load("./perlin.png");
+perlinTexture.wrapS = THREE.RepeatWrapping;
+perlinTexture.wrapT = THREE.RepeatWrapping;
+
+// Material
+const smokeMaterial = new THREE.ShaderMaterial({
+    vertexShader: coffeeSmokeVertexShader,
+    fragmentShader: coffeeSmokeFragmentShader,
+    uniforms: {
+        uTime: new THREE.Uniform(0),
+        uPerlinTexture: new THREE.Uniform(perlinTexture),
+        uColor: {
+            value: new THREE.Color(params.smokeColor.r, params.smokeColor.g, params.smokeColor.b),
+        },
+        uMouse: { value: new THREE.Vector2() },
+    },
+    transparent: true,
+    side: THREE.DoubleSide,
+    depthWrite: false,
+    // wireframe: true,
+});
+
+// Mesh
+const smoke = new THREE.Mesh(smokeGeometry, smokeMaterial);
+smoke.position.y = 1.83;
+scene.add(smoke);
 
 /**
  * Animate
  */
-const clock = new THREE.Clock()
+const clock = new THREE.Clock();
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime();
+
+    // Update smoke
+    smokeMaterial.uniforms.uTime.value = elapsedTime;
 
     // Update controls
-    controls.update()
+    controls.update();
 
     // Render
-    renderer.render(scene, camera)
+    renderer.render(scene, camera);
 
     // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
+    window.requestAnimationFrame(tick);
+};
 
-tick()
+tick();
